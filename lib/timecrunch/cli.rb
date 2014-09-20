5b7f23809f396e6e1d28659a4d7227bd72cb3995
Chris Keathley <spyc3r@gmail.com>
2014-09-20 08:29:30 -0400

    Add default 0 minutes if minutes aren't supplied
    It would be convenient to not have to include minutes if all you want is
    just to use the seconds argument.

--- lib/timecrunch/cli.rb
+++ lib/timecrunch/cli.rb
@@ -5,7 +5,7 @@ module Timecrunch
 
     desc "start MINUTES", "Starts a new timer for MINUTES"
     option :s, :banner => "seconds"
-    def start(minutes)
+    def start(minutes=0)
       seconds = options[:s].to_i if options[:s]
       minutes = minutes.to_i
       times = {
